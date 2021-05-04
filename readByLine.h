0d12c415d5b5a513a3156a4e62904a05c6bfc947
Desjajja <rottenlemon@qq.com>
2021-05-04 13:19:00 +0800

    delete redundant

--- readByLine.h
+++
@@ -1,6 +0,0 @@
-#if !defined(_READBYLINE_)
-#define _READBYLINE_
-
-int readByLine(const char * path, const char * line_num);
-
-#endif // _READBYLINE_
--- testReadByNum.c
+++
@@ -1,6 +0,0 @@
-#include "./readByLine.h"
-int main(int argc, char const *argv[])
-{
-	readByLine(argv[1], argv[2]);
-	return 0;
-}
