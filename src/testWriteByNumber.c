7d3cb9434055fb8a6df9bfc5b3eae676e2401c0f
Desjajja <rottenlemon@qq.com>
2021-05-05 16:30:17 +0800

    Not working yet

--- src/testWriteByNumber.c
+++ src/testWriteByNumber.c
@@ -7,8 +7,9 @@ int main(int argc, char const *argv[])
 {
 	char * iread = NULL;
 	char * iwrite = NULL;
-	char cread, cwrite, ch;
+	char cread, cwrite, ch, option;
 	char cline_content[32] = {0};
+	char buf_restore[MAX_ROW][32];
 	printf("choose one line to read from: ");
 	scanf("%c", &cread);
 	ch = getchar();
@@ -21,7 +22,14 @@ int main(int argc, char const *argv[])
 	scanf("%c", &cwrite);
 	ch = getchar();
 	iwrite = &cwrite;
-	writeByLine(argv[1], cline_content, iwrite);
+	writeByLine(argv[1], cline_content, iwrite, buf_restore, 0);
 	// writeByLine("test.txt", cline_content, iwrite);
+	printf("\nUndo:Yes(y)/No(n)");
+	scanf("%d", &option);
+	ch = getchar();
+	if(option == "Y" | option == "y")
+	{
+		writeByLine(argv[1], cline_content, iwrite, buf_restore, 1);	
+	}
 	return 0;
 }
